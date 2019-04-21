4675271c35e7723f33faf2f7ad96661bd6293f1a
khh <kamen.hristov.98@gmail.com>
2019-04-21 23:53:40 +0300

    updated crawler

--- Week5/Day2/MusicLibrary/music_crawler.py
+++ Week5/Day2/MusicLibrary/music_crawler.py
@@ -1,8 +1,43 @@
-from mutagen.mp3 import MP3
-from mutagen.easyid3 import EasyID3
-from mutagen.id3 import ID3
-import mutagen
+import os
+from mutagen.mp3 import EasyMP3
 
-song = MP3('Music/Van Halen - Panama.mp3')
-[print(k, v) for k, v in song.__dict__.items()]
-# print(dir(song))
+
+MUSIC_DIR = 'Music'
+
+class MusicCrawler:
+    """Each crawler contains the path to the directory to be crawled and the specific methods.
+    Current limitaions: 
+     - only works mp3 files.
+     - doesn't craw nested directories"""
+
+    AUDIO_FORMATS = ['.mp3'] 
+    def __init__(self, music_dir):
+        self._music_dir = music_dir
+
+    @property
+    def music_dir(self):
+        """Returns the absolute path representation of the directory to be crawled."""
+        return os.path.abspath(self._music_dir)
+
+    @music_dir.setter
+    def music_dir(self, path):
+        """Checks if the specified path is valid, if not - raises an error."""
+        if not os.path.isdir(path):
+            raise Exception("Directory specified not found!")
+        return path
+
+    def crawl(self):
+        """Crawls the specified directory containg audiо files."""
+        for file in os.listdir(self._music_dir):
+            root, ext = os.path.splitext
+            if ext in __class__.AUDIO_FORMATS:
+                #TODO finish
+                pass
+
+
+def main():
+    c = MusicCrawler(MUSIC_DIR)
+    print(c.music_dir)
+
+if __name__ == "__main__":
+    main()

05e5ec6f5529dad8285da897682dfccd241d6c23
khh <kamen.hristov.98@gmail.com>
2019-04-30 14:47:26 +0300

    changed graph_builder do data_builder

---
+++ Week08/data_builder.py
@@ -0,0 +1,106 @@
+import pprint
+from collections import deque
+
+
+class DataBuilder:
+    """Builds a list containing the elements of the given data
+    in a specific order according to the algorithm (DFS, BFS) used.
+    data = [(dict key, dict data)]"""
+    def __init__(self, data):
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data
+
+    def dfs(self):
+        """Returns a list containing the objects from the data using the DFS algorithm.
+        Gets all the children of each object, when the last child is reached, starts backtracking."""
+        dfs_struct = []
+        def dfs_helper(data, struct):
+            for obj in data:
+                dfs_struct.append((obj, data[obj]))       
+                if DataBuilder.is_dict(data[obj]):
+                    dfs_helper(data[obj], obj)
+                elif DataBuilder.is_iterable(data[obj]):
+                    iterables_helper(data[obj], struct)
+            return struct
+
+        def iterables_helper(iterable, struct):
+            """Calls @dfs_helper method if the object is of type dict, else
+            calls self recursively if @obj is an iterable (list, set, tuple..)."""
+            for obj in iterable:
+                if DataBuilder.is_dict(obj):
+                    dfs_helper(obj, struct)
+                elif DataBuilder.is_iterable(obj):
+                    iterables_helper(obj, struct)
+            return None
+
+
+        return dfs_helper(self.data, dfs_struct)
+
+    def bfs(self):
+        """Returns a list containing the objects from the data using the BFS algorithm,
+        gets all the object at each tree level, when the last object in the level is reached, 
+        proceeds to the next one."""
+        queue, bfs_struct = deque([self.data]), []
+        def bfs_helper(q, struct):
+            data = q.popleft()
+            for obj in data:
+                bfs_struct.append((obj, data[obj]))
+                if DataBuilder.is_dict(data[obj]): # and data[obj] not in q:
+                    q.append(data[obj])
+                elif DataBuilder.is_iterable(data[obj]): # and data[obj] not in q::
+                    iterables_helper(data[obj], queue)
+            if q:
+                bfs_helper(q, struct)
+            return struct
+        
+        def iterables_helper(iterable, queue):
+            """Enques the object if it's a dict or
+            calls self recursively if @obj is an iterable (list, set, tuple..)."""
+            for obj in iterable:
+                if DataBuilder.is_dict(obj):
+                   queue.append(obj)
+                elif DataBuilder.is_iterable(obj):
+                    iterables_helper(obj, queue)
+            return None
+
+        return bfs_helper(queue, bfs_struct)
+
+
+    @staticmethod
+    def is_dict(obj):
+        return isinstance(obj, dict)
+    
+    @staticmethod
+    def is_iterable(obj):
+        if isinstance(obj, str):
+            return False
+        try:
+            iter(obj)
+            return True
+        except TypeError:
+            return False
+
+            
+    @staticmethod
+    def pprint_data(data):
+        pprint.pprint(data, indent=1, width=10) 
+
+
+if __name__ == "__main__":
+    data = {
+        'a': [
+            {
+                'a1': 1,
+                'a2': 2
+            }
+        ],
+        'b': 2
+    }
+
+    gb = DataBuilder(data)
+    print(gb.dfs())
+    print('')
+    print(gb.bfs())

6501c133ad30dbfde5e3dba7393e7657aee21597
khh <kamen.hristov.98@gmail.com>
2019-05-07 00:51:18 +0300

    changed strucure of setUp method

--- Week08/test_deepops.py
+++ Week08/test_deepops.py
@@ -5,9 +5,9 @@ from datagenerator import DataGenerator
 
 class TestDeepOps(unittest.TestCase):
     def setUp(self):
-        self.simple_dict = DataGenerator({'a': 1, 'b': 2})
+        self.simple_dict = {'a': 1, 'b': 2}
         
-        self.nested_dicts = DataGenerator({
+        self.nested_dicts = {
             'a': {
                 'a': 1,
                 'b': {
@@ -17,11 +17,11 @@ class TestDeepOps(unittest.TestCase):
             'b': {
                 'b': 2
             }
-        })
+        }
 
-        self.simple_list_with_dict = DataGenerator([{'a': 1, 'b': 2}])
+        self.simple_list_with_dict = [{'a': 1, 'b': 2}]
 
-        self.nested_dicts_and_lists = DataGenerator([
+        self.nested_dicts_and_lists = [
             {
                 'a': 1,
                 'b': [
@@ -35,7 +35,7 @@ class TestDeepOps(unittest.TestCase):
                     {'b': 2}
                 ]
             }
-        ])
+        ]
 
     # ----------------------- DFS TESTS --------------------------
     # -------------------- deep_find DFS tests -------------------
@@ -135,7 +135,7 @@ class TestDeepOps(unittest.TestCase):
         do_obj = DeepOps(self.simple_dict)
         do_obj.deep_update('a', 2)
         expected_result = {'a': 2, 'b': 2}
-        self.assertEqual(self.simple_dict.data, expected_result)
+        self.assertEqual(self.simple_dict, expected_result)
 
     def test_deep_update_nested_dicts_dfs(self):
         do_obj = DeepOps(self.nested_dicts)
@@ -147,13 +147,13 @@ class TestDeepOps(unittest.TestCase):
             },
             'b': 2
         }
-        self.assertEqual(self.nested_dicts.data, expected_result)
+        self.assertEqual(self.nested_dicts, expected_result)
     
     def test_deep_update_simple_list_with_dict_dfs(self):
         do_obj = DeepOps(self.simple_list_with_dict)
         do_obj.deep_update('b', 3)
         expected_result = [{'a': 1, 'b': 3}]
-        self.assertEqual(self.simple_list_with_dict.data, expected_result)
+        self.assertEqual(self.simple_list_with_dict, expected_result)
 
     def test_deep_update_nested_dicts_and_lists_dfs(self):
         do_obj = DeepOps(self.nested_dicts_and_lists)
@@ -167,7 +167,7 @@ class TestDeepOps(unittest.TestCase):
                 'b' : 1
             }
         ]
-        self.assertEqual(self.nested_dicts_and_lists.data, expected_result)
+        self.assertEqual(self.nested_dicts_and_lists, expected_result)
     
     
 if __name__ == "__main__":

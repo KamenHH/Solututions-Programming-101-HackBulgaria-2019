a0bf4c140bdb0a6864815386d663d0d632e5a6b7
khh <kamen.hristov.98@gmail.com>
2019-05-07 10:51:28 +0300

    changed indentation

--- Week09/Day1/Relations/query1.sql
+++ Week09/Day1/Relations/query1.sql
@@ -1,5 +1,5 @@
 --Gets all stars who are male and have played in 'Terms of Endearment'
 SELECT STARNAME FROM STARSIN
-INNER JOIN MOVIESTAR
-ON STARNAME = NAME
+    INNER JOIN MOVIESTAR
+        ON STARNAME = NAME
 WHERE GENDER == 'M' AND MOVIETITLE == 'Terms of Endearment';

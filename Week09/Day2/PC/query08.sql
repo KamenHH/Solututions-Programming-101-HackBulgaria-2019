369971b9b9b261e502951b7d635ce14fbe88b175
khh <kamen.hristov.98@gmail.com>
2019-05-11 20:20:31 +0300

    fixed broken query

--- Week09/Day2/PC/query08.sql
+++ Week09/Day2/PC/query08.sql
@@ -1,9 +1,8 @@
 --Gets those manufacturers who have made 
 --more than 3 distinct pc models
 
-SELECT product.maker, COUNT(DISTINCT pc.model) as distinct_models
-    FROM pc
-    INNER JOIN product
-        ON pc.model = product.model
-GROUP BY product.maker
-    HAVING distinct_models > 3;
+SELECT maker
+    FROM product
+        WHERE type = 'PC'
+    GROUP BY maker
+        HAVING COUNT(model) >= 3;
